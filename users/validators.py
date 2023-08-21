7f8f8c9edb2d3430bc1e3d8ff2455aee70328ea0
Runn1dDefault <runniddefault@gmail.com>
2023-08-21 22:24:51 +0600

    fixing qty words validation on full_name field

--- users/validators.py
+++ users/validators.py
@@ -3,5 +3,4 @@ from django.utils.translation import gettext_lazy
 
 
 def validate_full_name(value: str):
-    if len(value.split()) != 2:
-        raise ValidationError(gettext_lazy('full_name must contains two params: first_name last_name'))
+    pass
