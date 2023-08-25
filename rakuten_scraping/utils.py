2f9aea04b811752e540c318cbe13bee6a4763cb7
Runn1dDefault <runniddefault@gmail.com>
2023-08-25 22:04:49 +0600

    fixing delay of rakuten requests and added func for collection products with converting japan date string to date

--- rakuten_scraping/utils.py
+++ rakuten_scraping/utils.py
@@ -1,3 +1,8 @@
+import random
+import time
+from datetime import datetime
+
+import numpy as np
 from typing import Any
 
 from product.models import Genre
@@ -6,14 +11,33 @@ from services.clients.rakuten import RakutenClient
 
 
 class RakutenRequest:
-    def __init__(self):
+    def __init__(self, delay: int = None, randomize_delay: bool = True):
+        self.randomize_delay = randomize_delay
+        self.delay = delay
         self.db_client = Oauth2Client.objects.free_client()
         self.client = RakutenClient(app_id=self.db_client.app_id, partner_id=self.db_client.partner_id or None)
 
     def close(self):
-        self.db_client.set_free()
+        st = time.monotonic()
+        time.sleep(self._get_delay())
+        print(f'Set Free {time.monotonic() - st}')
+        Oauth2Client.objects.set_free(self.db_client.id, self.db_client.app_id)
+
+    def _random_delay(self) -> float:
+        return random.choice(list(np.arange(self.delay, self.delay * 1.5, 0.1)))
+
+    def _get_delay(self) -> float:
+        if self.delay and self.randomize_delay:
+            rand_delay = self._random_delay()
+            print('Random Delay is %s' % rand_delay)
+            return rand_delay
+        return self.delay
 
     def __enter__(self):
+        Oauth2Client.objects.set_busy(
+            client_id=self.db_client.id,
+            app_id=self.db_client.app_id
+        )
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -27,3 +51,34 @@ def get_db_genre_by_data(data: dict[str, Any], fields: dict[str, str]):
 
     return Genre(**query)
 
+
+def get_release_date_from_ja(ja_release_date: str):
+    release_date = None
+    date_format = None
+    if '年' in ja_release_date and '月' in ja_release_date:
+        date_format = "%Y年%m月"
+    if '日' in ja_release_date:
+        date_format = "%Y年%m月%d日"
+
+    if date_format:
+        release_date = datetime.strptime(ja_release_date, date_format).date()
+    return release_date
+
+
+def collect_product_fields(product_data: dict[str, Any]):
+    image_url = product_data.get('mediumImageUrl') or product_data.get('smallImageUrl')
+    release_date = get_release_date_from_ja(product_data['releaseDate'])
+
+    return dict(
+        number=product_data['productNo'],
+        name=product_data['productName'],
+        description=product_data['productCaption'],
+        brand_name=product_data['brandName'],
+        rank=product_data['rank'],
+        price=product_data['maxPrice'],
+        count=product_data['itemCount'],
+        image_url=image_url.split('?')[0] if image_url else None,
+        product_url=product_data['productUrlPC'],
+        release_date=release_date,
+        marker_code=product_data['makerCode'] or None
+    )
