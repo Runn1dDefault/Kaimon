a2a96016fe1fbc44f0588a137ae8ac4502e5de04
Runn1dDefault <runniddefault@gmail.com>
2023-08-25 22:02:55 +0600

    added addition functions

--- product/utils.py
+++ product/utils.py
@@ -1,8 +1,36 @@
-import hashlib
-from django.utils import timezone
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+from rest_framework.exceptions import ParseError
 
 
-def generate_product_id():
-    hashlib.sha256()
-    timezone.now().timestamp()
-    return
+def get_genre_parents_tree(current_genre) -> list[int]:
+    collected_parents = [current_genre.id]
+    for fk_parent in current_genre.parents.all():
+        collected_parents.extend(get_genre_parents_tree(fk_parent.parent))
+
+    return collected_parents
+
+
+def get_last_children(current_genre) -> list[int]:
+    last_children = []
+
+    for fk_child in current_genre.children.all():
+        child = fk_child.child
+        if not child.children.exists():
+            last_children.append(child.id)
+            continue
+
+        last_children.extend(get_last_children(child))
+    return last_children
+
+
+def get_request_lang(request):
+    lang = str(request.query_params.get(settings.LANGUAGE_QUERY, 'ja')).lower()
+    if lang not in settings.SUPPORTED_LANG:
+        raise ParseError(detail=_("Language %s does not support!") % lang)
+    return lang
+
+
+def get_field_by_lang(field_name: str, lang: str):
+    if lang in settings.SUPPORTED_LANG:
+        return f'{field_name}_{lang}' if lang != 'ja' else field_name
