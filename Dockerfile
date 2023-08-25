6af4f72a9d8387f1570bc6ede0b6dfbd49f144d8
Runn1dDefault <runniddefault@gmail.com>
2023-08-25 23:12:28 +0600

    remove netcat because server not supported

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3.11
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-RUN apt-get update && apt-get install -y netcat
+RUN apt-get update
 
 WORKDIR /app
 
--- deploy/sh/entrypoint.sh
+++ deploy/sh/entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-echo "Waiting for postgres..."
-  while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-    sleep 0.1
-  done
-echo "Postgres started"
-
-
 pip install --upgrade pip
 
 echo 'Start making migrations...'
