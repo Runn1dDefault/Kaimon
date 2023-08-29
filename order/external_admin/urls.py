2639be1b3df15c02be8a7d846acd16ae2b987d21
dinnurnarmatov <dinnur.narmatov@data-ox.com>
2023-08-30 03:49:57 +0600

    remove empty space

--- order/external_admin/urls.py
+++ order/external_admin/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, re_path
 from .views import AdminOrderListView, AdminOrderSearchView, update_order_to_delivered, update_order_to_success
 
 
-
 urlpatterns = [
     path('orders/', AdminOrderListView.as_view(), name='admin_orders_list'),
     path('orders/search/', AdminOrderSearchView.as_view(), name='admin_orders_search'),
