2f12fd69c2aa85acd54736ed8636e57115c4e067
Runn1dDefault <runniddefault@gmail.com>
2023-08-31 18:39:11 +0600

    separating update statuses of order

--- order/permissions.py
+++ order/permissions.py
@@ -4,9 +4,12 @@ from order.models import Order
 
 
 class OrderPermission(BasePermission):
-    DELETE_STATUSES = [Order.Status.pending, Order.Status.success]
+    UPDATE_STATUSES = [Order.Status.pending]
+    DELETE_STATUSES = [Order.Status.pending, Order.Status.canceled, Order.Status.rejected]
 
     def has_object_permission(self, request, view, obj):
-        if request.METHOD in ['PUT', 'PATCH', 'DELETE'] and obj.status not in self.DELETE_STATUSES:
+        if request.METHOD in ['PUT', 'PATCH'] and obj.status not in self.UPDATE_STATUSES:
+            return False
+        elif request.METHOD == 'DELETE' and obj.status not in self.DELETE_STATUSES:
             return False
         return obj.delivery_address.user == request.user
