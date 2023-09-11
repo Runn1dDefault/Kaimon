d943d05b04f7c69a06845d9255956072d9c1f263
Runn1dDefault <runniddefault@gmail.com>
2023-09-11 22:51:09 +0600

    added UserAnalyticsQuerySet

---
+++ users/querysets.py
@@ -0,0 +1,23 @@
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models import Count, F
+from django.db.models.functions import JSONObject, TruncDate
+
+from utils.queryset import AnalyticsQuerySet
+from utils.types import AnalyticsFilter
+
+
+class UserAnalyticsQuerySet(AnalyticsQuerySet):
+    def get_joined_analytics(self, by: AnalyticsFilter):
+        return self.values(date=by.value('date_joined')).annotate(
+            users=ArrayAgg(
+                JSONObject(
+                    id=F('id'),
+                    email=F('email'),
+                    is_active=F('is_active'),
+                    registration_payd=F('registration_payed'),
+                    date_joined=TruncDate('date_joined'),
+                    image=F('image')
+                )
+            ),
+            count=Count('id')
+        )
