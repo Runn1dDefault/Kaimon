233c18f2da7b1f0a0988a209d9daa329435ec8a7
dinnurnarmatov <dinnur.narmatov@data-ox.com>
2023-09-15 18:59:16 +0600

    removed TagByGroupSerializer and TagSerializer and changed get_tags_info. Removed product_name from ProductReviewSerializer

--- product/serializers.py
+++ product/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import empty
 
 from currencies.serializers import ConversionField
 from users.serializers import UserProfileSerializer
@@ -14,13 +15,12 @@ class GenreSerializer(LangSerializerMixin, serializers.ModelSerializer):
         translate_fields = ('name',)
 
 
-class ProductReviewSerializer(LangSerializerMixin, serializers.ModelSerializer):
-    user = UserProfileSerializer(hide_fields=['role', 'email'], read_only=True)
-    product_name = serializers.SerializerMethodField(read_only=True)  # TODO: delete this field if not used on front
+class ProductReviewSerializer(serializers.ModelSerializer):
+    user = UserProfileSerializer(hide_fields=('role', 'email'), read_only=True)
 
     class Meta:
         model = ProductReview
-        fields = ('id', 'user', 'product', 'product_name', 'rank', 'comment', 'created_at')
+        fields = ('id', 'user', 'product', 'rank', 'comment', 'created_at')
         extra_kwargs = {'product': {'write_only': True, 'required': True}}
 
     def validate(self, attrs):
@@ -28,11 +28,6 @@ class ProductReviewSerializer(LangSerializerMixin, serializers.ModelSerializer):
         assert user.is_authenticated, f'Serializer {self.__class__.__name__} not available to {user.__class__.__name__}'
         attrs['user'] = user
         return attrs
-
-    def get_product_name(self, instance):
-        product = instance.product
-        translate_field = self.get_translate_field('name')
-        return getattr(product, translate_field, None) or product.name
 
 
 class ProductListSerializer(LangSerializerMixin, serializers.ModelSerializer):
@@ -50,32 +45,11 @@ class ProductListSerializer(LangSerializerMixin, serializers.ModelSerializer):
         kwargs['many'] = True
         super().__init__(instance=instance, data=None, **kwargs)
 
+    def update(self, instance, validated_data):
+        raise NotImplementedError('`update()` not implemented.')
 
-class TagSerializer(LangSerializerMixin, serializers.ModelSerializer):
-    class Meta:
-        model = Tag
-        fields = ('id', 'name')
-        translate_fields = ('name',)
-
-
-class TagByGroupSerializer(LangSerializerMixin, serializers.ModelSerializer):
-    tags = serializers.SerializerMethodField(read_only=True)
-
-    def __init__(self, instance=None, data=None, tag_ids: list[int] = None, **kwargs):
-        super().__init__(instance=instance, data=data, **kwargs)
-        self.tag_ids = tag_ids
-
-    class Meta:
-        model = TagGroup
-        fields = ('id', 'name', 'tags')
-        translate_fields = ('name',)
-
-    def get_tags(self, instance):
-        if self.tag_ids:
-            queryset = instance.tags.filter(id__in=self.tag_ids)
-        else:
-            queryset = instance.tags.all()
-        return TagSerializer(instance=queryset, many=True, context=self.context).data
+    def create(self, validated_data):
+        raise NotImplementedError('`create()` not implemented.')
 
 
 class ProductRetrieveSerializer(LangSerializerMixin, serializers.ModelSerializer):
@@ -89,22 +63,21 @@ class ProductRetrieveSerializer(LangSerializerMixin, serializers.ModelSerializer
         fields = ('id', 'name', 'price', 'sale_price', 'availability', 'avg_rank', 'reviews_count', 'image_urls',
                   'created_at', 'genre_id', 'description', 'tags_info')
         translate_fields = ('name', 'description')
-        currency_convert_fields = ('sale_price', 'price',)
 
     def __init__(self, instance, **kwargs):
         assert isinstance(instance, Product)
         kwargs['many'] = False
-        super().__init__(instance=instance, data=None, **kwargs)
+        super().__init__(instance=instance, data=empty, **kwargs)
 
     def get_tags_info(self, instance):
-        tags_queryset = Tag.objects.filter(producttag__product_id=instance.id)
-        if not tags_queryset.exists():
+        tags_fk = instance.tags.all()
+        if not tags_fk.exists():
             return
-        group_ids = tags_queryset.order_by('group_id').distinct('group_id').values_list('group_id', flat=True)
+
+        group_ids = tags_fk.order_by('tag__group_id').distinct('tag__group_id').values_list('tag__group_id', flat=True)
         groups_queryset = TagGroup.objects.filter(id__in=group_ids)
-        return TagByGroupSerializer(
-            tag_ids=tags_queryset.values_list('id', flat=True),
-            instance=groups_queryset,
-            many=True,
-            context=self.context
-        ).data
+        tag_translate_field = self.get_translate_field('name')
+        return groups_queryset.tags_list(
+            name_field=tag_translate_field,
+            tag_ids=tags_fk.values_list('tag_id', flat=True)
+        )
