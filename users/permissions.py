b74e4c91b7a6814f4349394912db1a0c4f47121a
dinnurnarmatov <dinnur.narmatov@data-ox.com>
2023-09-17 16:08:42 +0600

    added user email confirmed permission

--- users/permissions.py
+++ users/permissions.py
@@ -9,6 +9,12 @@ class RegistrationPayedPermission(IsAuthenticated):
         return super().has_permission(request, view) and request.user.registration_payed
 
 
+class EmailConfirmedPermission(IsAuthenticated):
+
+    def has_permission(self, request, view):
+        return super().has_permission(request, view) and request.user.email_confirmed
+
+
 class IsDirectorPermission(BasePermission):
     def has_permission(self, request, view):
         return request.user and request.user.is_director
