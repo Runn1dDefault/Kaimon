ef4f6112d9fa98c58421a9ca98f5c01351efa350
dinnurnarmatov <dinnur.narmatov@data-ox.com>
2023-09-18 13:38:06 +0600

    remove AuthorPermission

--- order/permissions.py
+++ order/permissions.py
@@ -1,15 +1,6 @@
 from rest_framework.permissions import BasePermission
 
-from order.models import Order
-
-
-class AuthorPermission(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_authenticated and request.user == obj.user
-
 
 class OrderPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if obj.delivery_address.user != request.user:
-            return False
-        return True
+        return obj.delivery_address.user == request.user or request.user.is_superuser
