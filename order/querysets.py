acc91e36d8cf69b2971cbf1c554b056f4b9b9a6e
dinnurnarmatov <dinnur.narmatov@data-ox.com>
2023-09-18 13:43:29 +0600

    sale_price_case to global func

--- order/querysets.py
+++ order/querysets.py
@@ -1,35 +1,41 @@
 from django.contrib.postgres.aggregates import ArrayAgg
-from django.db.models import F, Case, When, Value, ExpressionWrapper, Q
+from django.db.models import F, Case, When, Value, Q, Sum
 from django.db.models.functions import JSONObject, Round
 
 from utils.querysets import AnalyticsQuerySet
 from utils.types import AnalyticsFilter
 
 
+def sale_price_calc_case(currency_field: str = None, from_receipt_obj: bool = True):
+    if from_receipt_obj is False:
+        discount_field = 'receipts__discount'
+        quantity_field = 'receipts__quantity'
+        unit_price_field = 'receipts__unit_price'
+    else:
+        discount_field = 'discount'
+        quantity_field = 'quantity'
+        unit_price_field = 'unit_price'
+
+    discount_formula = ((F(discount_field) / F(unit_price_field)) * Value(100.0)) * F(quantity_field)
+    simple_formula = F(unit_price_field) * F(quantity_field)
+
+    if currency_field is not None:
+        discount_formula *= F(currency_field)
+        simple_formula *= F(currency_field)
+
+    return Case(When(**{discount_field: 0}, then=Round(simple_formula, precision=2)),
+                When(**{discount_field + '__gt': 0}, then=Round(discount_formula, precision=2)))
+
+
 class OrderAnalyticsQuerySet(AnalyticsQuerySet):
-    sale_formula = F('receipts__unit_price') - (F('receipts__discount') * F('receipts__unit_price') / Value(100.0))
-    receipts_sale_formula = F('unit_price') - (F('discount') * F('unit_price') / Value(100.0))
+    def total_prices(self):
+        return self.annotate(
+            yen=Sum(sale_price_calc_case(from_receipt_obj=False)),
+            som=Sum(sale_price_calc_case('yen_to_som', from_receipt_obj=False)),
+            dollar=Sum(sale_price_calc_case('yen_to_usd', from_receipt_obj=False))
+        )
 
-    def sale_price_case(self, currency_field: str = None, from_receipts: bool = False):
-        if from_receipts:
-            with_discount_formula = self.receipts_sale_formula * F('quantity')
-            without_discount_formula = F('unit_price') * F('quantity')
-            discount_zero_exp = Q(discount=0)
-            discount_gt_zero_exp = Q(discount__gt=0)
-        else:
-            discount_zero_exp = Q(receipts__discount=0)
-            discount_gt_zero_exp = Q(receipts__discount__gt=0)
-            with_discount_formula = self.sale_formula * F('receipts__quantity')
-            without_discount_formula = F('receipts__unit_price') * F('receipts__quantity')
-
-        if currency_field is not None:
-            with_discount_formula *= F(currency_field)
-            without_discount_formula *= F(currency_field)
-
-        return Case(When(discount_zero_exp, then=Round(without_discount_formula, precision=2)),
-                    When(discount_gt_zero_exp, then=Round(with_discount_formula, precision=2)))
-
-    def get_analytics(self, by: AnalyticsFilter):
+    def by_dates(self, by: AnalyticsFilter):
         return self.values(date=by.value('created_at')).annotate(
             receipts_info=ArrayAgg(
                 JSONObject(
@@ -43,7 +49,7 @@ class OrderAnalyticsQuerySet(AnalyticsQuerySet):
                     status=F('status')
                 )
             ),
-            sale_prices_yen=ArrayAgg(self.sale_price_case(from_receipts=False)),
-            sale_prices_som=ArrayAgg(self.sale_price_case('yen_to_som', from_receipts=False)),
-            sale_prices_dollar=ArrayAgg(self.sale_price_case('yen_to_usd', from_receipts=False))
+            yen=Sum(sale_price_calc_case(from_receipt_obj=False)),
+            som=Sum(sale_price_calc_case('yen_to_som', from_receipt_obj=False)),
+            dollar=Sum(sale_price_calc_case('yen_to_usd', from_receipt_obj=False))
         )
