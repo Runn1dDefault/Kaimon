39560272f938180392314d70556d1104dfad5013
Runn1dDefault <runniddefault@gmail.com>
2023-09-24 17:34:27 +0600

    added EmailConfirmedPermission to permission_classes

--- order/views.py
+++ order/views.py
@@ -1,11 +1,9 @@
 from django.db import transaction
-from rest_framework import viewsets, mixins
-from rest_framework.decorators import action
+from rest_framework import viewsets, mixins, parsers
 
-from product.models import Product
 from product.serializers import ProductListSerializer
 from users.filters import FilterByUser
-from users.permissions import RegistrationPayedPermission
+from users.permissions import RegistrationPayedPermission, EmailConfirmedPermission
 from utils.filters import ListFilterFields
 from utils.paginators import PagePagination
 
@@ -16,7 +14,7 @@ from .serializers import DeliveryAddressSerializer, OrderSerializer
 
 class DeliveryAddressViewSet(viewsets.ModelViewSet):
     queryset = DeliveryAddress.objects.filter(as_deleted=False)
-    permission_classes = [RegistrationPayedPermission]
+    permission_classes = [EmailConfirmedPermission, RegistrationPayedPermission]
     serializer_class = DeliveryAddressSerializer
     pagination_class = PagePagination
     filter_backends = [FilterByUser]
@@ -37,7 +35,8 @@ class OrderViewSet(
     viewsets.GenericViewSet
 ):
     queryset = Order.objects.all()
-    permission_classes = [RegistrationPayedPermission, OrderPermission]
+    parser_classes = (parsers.JSONParser,)
+    permission_classes = [EmailConfirmedPermission, RegistrationPayedPermission, OrderPermission]
     serializer_class = OrderSerializer
     product_serializer_class = ProductListSerializer
     pagination_class = PagePagination
@@ -51,10 +50,4 @@ class OrderViewSet(
     def get_queryset(self):
         return super().get_queryset().filter(delivery_address__user=self.request.user)
 
-    @action(methods=['GET'], detail=True)
-    def order_products(self, request, **kwargs):
-        product_ids = self.get_object().receipts.values_list('product_id', flat=True)
-        products = Product.objects.filter(id__in=product_ids, is_active=True, availability=True)
-        page = self.paginate_queryset(products)
-        serializer = self.product_serializer_class(page, many=True, context=self.get_serializer_context())
-        return self.get_paginated_response(serializer.data)
+    # TODO: add cancel order if order.status == Order.Status.pending
