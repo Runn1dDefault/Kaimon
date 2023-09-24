4ebb48ec188212e80290278cd07e09470682d8ca
Runn1dDefault <runniddefault@gmail.com>
2023-09-24 17:38:47 +0600

    added count, total_price to analytics method and added useful comment to sale_price_calc_case func

--- order/querysets.py
+++ order/querysets.py
@@ -1,29 +1,32 @@
 from django.contrib.postgres.aggregates import ArrayAgg
-from django.db.models import F, Case, When, Value, Sum
+from django.db.models import F, Case, When, Value, Sum, DecimalField, Count
 from django.db.models.functions import JSONObject, Round
 
 from utils.querysets import AnalyticsQuerySet
 
 
 def sale_price_calc_case(currency_field: str = None, from_receipt_obj: bool = True):
+    # if you change some field types to FloatField, you will need to make changes
     if from_receipt_obj is False:
         discount_field = 'receipts__discount'
-        quantity_field = 'receipts__quantity'
-        unit_price_field = 'receipts__unit_price'
+        quantity_field = F('receipts__quantity')
+        unit_price_field = F('receipts__unit_price')
     else:
         discount_field = 'discount'
-        quantity_field = 'quantity'
-        unit_price_field = 'unit_price'
+        quantity_field = F('quantity')
+        unit_price_field = F('unit_price')
 
-    discount_formula = ((F(discount_field) / F(unit_price_field)) * Value(100.0)) * F(quantity_field)
-    simple_formula = F(unit_price_field) * F(quantity_field)
+    hundred = Value(100.0, output_field=DecimalField())
+    discount_formula = (unit_price_field - (F(discount_field) * unit_price_field) / hundred) * quantity_field
+    simple_formula = unit_price_field * quantity_field
 
     if currency_field is not None:
         discount_formula *= F(currency_field)
         simple_formula *= F(currency_field)
-
-    return Case(When(**{discount_field: 0}, then=Round(simple_formula, precision=2)),
-                When(**{discount_field + '__gt': 0}, then=Round(discount_formula, precision=2)))
+    return Case(
+        When(**{discount_field: 0}, then=Round(simple_formula, precision=2)),
+        When(**{discount_field + '__gt': 0}, then=Round(discount_formula, precision=2))
+    )
 
 
 class OrderAnalyticsQuerySet(AnalyticsQuerySet):
@@ -45,10 +48,12 @@ class OrderAnalyticsQuerySet(AnalyticsQuerySet):
                     quantity=F('receipts__quantity'),
                     yen_to_som=F('yen_to_som'),
                     yen_to_dollar=F('yen_to_usd'),
-                    status=F('status')
+                    status=F('status'),
+                    total_price=sale_price_calc_case(from_receipt_obj=False)
                 )
             ),
             yen=Sum(sale_price_calc_case(from_receipt_obj=False)),
             som=Sum(sale_price_calc_case('yen_to_som', from_receipt_obj=False)),
-            dollar=Sum(sale_price_calc_case('yen_to_usd', from_receipt_obj=False))
+            dollar=Sum(sale_price_calc_case('yen_to_usd', from_receipt_obj=False)),
+            count=Count('id')
         )
