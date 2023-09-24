ccac03744e32af77d97a2eb8dfa129ce16d84bc5
Runn1dDefault <runniddefault@gmail.com>
2023-09-24 17:25:59 +0600

    added UserReviewListView, ProductReviewDestroyView and token verification is disabled if it is sent

--- product/urls.py
+++ product/urls.py
@@ -2,32 +2,34 @@ from django.urls import path, re_path
 
 from .views import (
     GenreListView, GenreChildrenView, GenreParentsView, TagByGenreListView,
-    ProductsListByGenreView, ProductRetrieveView,
-    ProductReviewCreateView, ProductReviewListView,
+    ProductsListByGenreView, ProductRetrieveView, ProductReviewDestroyView,
+    UserReviewListView, ProductReviewCreateView, ProductReviewListView,
     ReferenceListView,
 )
 
 genres_urlpatterns = [
-    path('genres/', GenreListView.as_view(), name='product_genre_list'),
-    re_path('^genres/(?P<id>.+)/children/$', GenreChildrenView.as_view(), name='product_genre_children_list'),
-    re_path('^genres/(?P<id>.+)/parents/$', GenreParentsView.as_view(), name='product_genre_parents_list'),
-    re_path('^genres/(?P<id>.+)/tags/$', TagByGenreListView.as_view(), name='product_genre_tags_list'),
+    path('genres/', GenreListView.as_view(), name='product-genre-list'),
+    re_path('^genres/(?P<id>.+)/children/$', GenreChildrenView.as_view(), name='product-genre-children-list'),
+    re_path('^genres/(?P<id>.+)/parents/$', GenreParentsView.as_view(), name='product-genre-parents-list'),
+    re_path('^genres/(?P<id>.+)/tags/$', TagByGenreListView.as_view(), name='product-genre-tags-list'),
 ]
 
 reviews_urlpatterns = [
-    path('review/', ProductReviewCreateView.as_view(), name='product_review_create'),
-    re_path('^(?P<id>.+)/reviews/', ProductReviewListView.as_view(), name='product_review_list'),
-]
-
-products_urlpatterns = [
-    re_path('^genres/(?P<id>.+)/products/$', ProductsListByGenreView.as_view(), name='product_list'),
-    # Important: product_detail must always be the last, otherwise it will overlap other addresses.
-    # also product_urlpatterns should also be at the end of urlpatterns
-    re_path('^(?P<id>.+)/$', ProductRetrieveView.as_view(), name='product_detail'),
+    path('my-reviews/', UserReviewListView.as_view(), name='product-user-reviews-list'),
+    path('reviews/', ProductReviewCreateView.as_view(), name='product-review-create'),
+    re_path('^(?P<id>.+)/reviews/', ProductReviewListView.as_view(), name='product-review-list'),
+    re_path('^reviews/(?P<id>.+)/$', ProductReviewDestroyView.as_view(), name='product-review-delete'),
 ]
 
 additions_urlpatterns = [
-    path('recommendations/', ReferenceListView.as_view(), name='product_recommendations_list'),
+    path('recommendations/', ReferenceListView.as_view(), name='product-recommendations-list'),
+]
+
+products_urlpatterns = [
+    re_path('^genres/(?P<id>.+)/products/$', ProductsListByGenreView.as_view(), name='product-list'),
+    # Important: product_detail must always be the last, otherwise it will overlap other addresses.
+    # also product_urlpatterns should also be at the end of urlpatterns
+    re_path('^(?P<id>.+)/$', ProductRetrieveView.as_view(), name='product-detail'),
 ]
 
 urlpatterns = additions_urlpatterns + genres_urlpatterns + reviews_urlpatterns + products_urlpatterns
