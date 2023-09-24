e87f533284c4c3a75ab4ac55e8d3dab0403d0b0d
Runn1dDefault <runniddefault@gmail.com>
2023-09-24 16:02:03 +0600

    added tags_data method and fixing ordering of order_by_popular and added user full name to ReviewAnalyticsQuerySet.by_dates

--- product/querysets.py
+++ product/querysets.py
@@ -10,6 +10,27 @@ class TagGroupQuerySet(QuerySet):
     def groups_with_tags(self):
         return self.annotate(tags_qty=Count('tags__id')).filter(tags_qty__gt=0)
 
+    def tags_data(self, tag_ids=None):
+        return self.values(group_id=F('id')).annotate(
+            group_name=F('name'),
+            group_name_ru=F('name_ru'),
+            group_name_en=F('name_en'),
+            group_name_tr=F('name_tr'),
+            group_name_ky=F('name_ky'),
+            group_name_kz=F('name_kz'),
+            tag_info=ArrayAgg(
+                JSONObject(
+                    id=F('tags__id'),
+                    name=F('tags__name'),
+                    name_ru=F('tags__name_ru'),
+                    name_en=F('tags__name_en'),
+                    name_tr=F('tags__name_tr'),
+                    name_ky=F('tags__name_ky'),
+                    name_kz=F('tags__name_kz'),
+                ), filter=Q(tags__id__in=tag_ids) if tag_ids else None
+            )
+        )
+
     def tags_list(self, name_field: str = 'name', tag_ids=None):
         return self.values(group_id=F('id'), group_name=F(name_field)).annotate(
             tag_info=ArrayAgg(
@@ -48,9 +69,9 @@ class GenreQuerySet(QuerySet):
 class ProductQuerySet(QuerySet):
     def order_by_popular(self):
         return self.annotate(
-            avg_rank=Avg('reviews__rank', filter=Q(reviews__is_active=True)),
+            average_rank=Avg('reviews__rank', filter=Q(reviews__is_active=True)),
             receipts_qty=Count('receipts__order_id', distinct=True)
-        ).order_by('receipts_qty', 'avg_rank')
+        ).order_by(F('receipts_qty').desc(nulls_last=True), F('average_rank').desc(nulls_last=True))
 
 
 class ReviewAnalyticsQuerySet(AnalyticsQuerySet):
@@ -59,7 +80,8 @@ class ReviewAnalyticsQuerySet(AnalyticsQuerySet):
             info=ArrayAgg(
                 JSONObject(
                     user_id=F('user__id'),
-                    user_email=F('user__email'),
+                    email=F('user__email'),
+                    name=F('user__full_name'),
                     comment=F('comment'),
                     rank=F('rank'),
                     created_at=F('created_at'),
