01366a39b61064883c20d5a8d88ad3c873a6b2f8
Runn1dDefault <runniddefault@gmail.com>
2023-10-04 17:33:06 +0600

    renaming restore urls

--- users/urls.py
+++ users/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('confirm/email/', ConfirmEmailView.as_view(), name='auth-email-confirm'),
     path('me-info/', UserInfoView.as_view(), name='auth-me-info'),
     path('me-info/update/', UpdateUserInfo.as_view(), name='auth-me-info-update'),
-    path('forgot-pwd/', RecoveryCodeView.as_view(), name='auth-restore-mailing-code'),
-    path('forgot-pwd-token/', RecoveryTokenView.as_view(), name='auth-restore-token'),
+    path('send-verification-code/', RecoveryCodeView.as_view(), name='auth-restore-mailing-code'),
+    path('pwd-update-token/', RecoveryTokenView.as_view(), name='auth-restore-token'),
     path('recovery/password/', RecoveryPasswordView.as_view(), name='auth-update-pwd')
 ]
