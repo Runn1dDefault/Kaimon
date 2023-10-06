143f02df3a0e6e5086d0d161969f2e87ff5907c4
dinnurnarmatov <dinnur.narmatov@data-ox.com>
2023-10-06 06:20:01 +0600

    added QuerysetPagination

--- product/paginators.py
+++ product/paginators.py
@@ -1,3 +1,6 @@
+from django.core.paginator import InvalidPage, Page
+from django.db.models import QuerySet
+from rest_framework.exceptions import NotFound
 from rest_framework.pagination import PageNumberPagination
 
 
@@ -5,3 +8,37 @@ class GenrePagination(PageNumberPagination):
     page_size = 40
     max_page_size = 40
     page_size_query_param = 'page_size'
+
+
+class QuerysetPagination(PageNumberPagination):
+    page_size = 10
+    max_page_size = 100
+    page_size_query_param = 'page_size'
+
+    def paginate_queryset(self, queryset, request, view=None) -> QuerySet | None:
+        page_size = self.get_page_size(request)
+        if not page_size:
+            return None
+
+        paginator = self.django_paginator_class(queryset, page_size)
+        page_number = self.get_page_number(request, paginator)
+        number = paginator.validate_number(page_number)
+        bottom = (number - 1) * paginator.per_page
+        top = bottom + paginator.per_page
+        if top + paginator.orphans >= paginator.count:
+            top = paginator.count
+
+        paginated_queryset = queryset[bottom:top]
+        try:
+            self.page = Page(paginated_queryset, number, paginator)
+        except InvalidPage as exc:
+            msg = self.invalid_page_message.format(
+                page_number=page_number, message=str(exc)
+            )
+            raise NotFound(msg)
+
+        if paginator.num_pages > 1 and self.template is not None:
+            # The browsable API should display pagination controls.
+            self.display_page_controls = True
+        self.request = request
+        return paginated_queryset
