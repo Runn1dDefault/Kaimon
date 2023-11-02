1eecac8ada26aa2b565886e756cd4698a4868683
Runn1dDefault <runniddefault@gmail.com>
2023-11-02 14:45:03 +0600

    added get_tags_for_product

--- product/utils.py
+++ product/utils.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.conf import settings
+from django.db import connection
 
 
 def increase_price(price):
@@ -41,3 +42,32 @@ def get_last_children(current_genre) -> list[int]:
 
         last_children.extend(get_last_children(child))
     return last_children
+
+
+def get_tags_for_product(product_id) -> list[dict[str, int | str]]:
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+            SELECT t.id as tag_id, t.name as tag_name, t.group_id as group_id, tg.name as group_name
+              FROM product_tag as t
+            JOIN product_taggroup as tg ON (t.group_id = tg.id)
+            INNER JOIN product_product_tags as pt ON (t.id = pt.tag_id)
+            WHERE pt.product_id = %s
+            """, (product_id,)
+        )
+        columns = [col[0] for col in cursor.description]
+        rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+    collected_data = {}
+    for row in rows:
+        group_id = row['group_id']
+        if group_id not in collected_data:
+            collected_data[group_id] = {
+                'group_id': group_id,
+                'group_name': row['group_name'],
+                'tags': []
+            }
+        collected_data[group_id]['tags'].append(
+            {'id': row['tag_id'], 'name': row['tag_name']}
+        )
+    return list(collected_data.values())
