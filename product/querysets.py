4f66e3d7889328f3597e49a5b6bc223f657099cf
Runn1dDefault <runniddefault@gmail.com>
2023-11-02 14:49:00 +0600

    removed unused dependencies

--- product/querysets.py
+++ product/querysets.py
@@ -7,73 +7,17 @@ from utils.types import AnalyticsFilter
 
 
 class TagGroupQuerySet(QuerySet):
-    def groups_with_tags(self):
-        return self.annotate(tags_qty=Count('tags__id')).filter(tags_qty__gt=0)
-
-    def tags_data(self, tag_ids=None):
-        return self.values(group_id=F('id')).annotate(
-            group_name=F('name'),
-            group_name_ru=F('name_ru'),
-            group_name_en=F('name_en'),
-            group_name_tr=F('name_tr'),
-            group_name_ky=F('name_ky'),
-            group_name_kz=F('name_kz'),
+    def tags_list(self, tag_ids=None):
+        return self.values(group_id=F('id'), group_name=F('name')).annotate(
             tags=ArrayAgg(
                 JSONObject(
                     id=F('tags__id'),
-                    name=F('tags__name'),
-                    name_ru=F('tags__name_ru'),
-                    name_en=F('tags__name_en'),
-                    name_tr=F('tags__name_tr'),
-                    name_ky=F('tags__name_ky'),
-                    name_kz=F('tags__name_kz'),
-                ), filter=Q(tags__id__in=tag_ids) if tag_ids else None
-            )
-        )
-
-    def tags_list(self, name_field: str = 'name', tag_ids=None):
-        return self.values(group_id=F('id'), group_name=F(name_field)).annotate(
-            tags=ArrayAgg(
-                JSONObject(
-                    id=F('tags__id'),
-                    name=F('tags__' + name_field)
+                    name=F('tags__name')
                 ),
                 filter=Q(tags__id__in=tag_ids) if tag_ids else None,
                 distinct=True  # required
             )
         )
-
-
-class GenreQuerySet(QuerySet):
-    def genre_info_with_relations(self, name_field: str = 'name'):
-        return self.values(
-            genre_id=F('id'),
-            genre_level=F('level'),
-            genre_name=F(name_field)
-        ).annotate(
-            parents=ArrayAgg(
-                JSONObject(
-                    genre_id=F('parents__parent__id'),
-                    genre_level=F('parents__parent__level'),
-                    genre_name=F('parents__parent__' + name_field)
-                ), distinct=True
-            ),
-            children=ArrayAgg(
-                JSONObject(
-                    genre_id=F('children__child__id'),
-                    genre_level=F('children__child__level'),
-                    genre_name=F('children__child__' + name_field)
-                ), distinct=True
-            ),
-        )
-
-
-class ProductQuerySet(QuerySet):
-    def order_by_popular(self):
-        return self.annotate(
-            average_rank=Avg('reviews__rank', filter=Q(reviews__is_active=True)),
-            receipts_qty=Count('receipts__order_id', distinct=True)
-        ).order_by(F('receipts_qty').desc(nulls_last=True), F('average_rank').desc(nulls_last=True))
 
 
 class ReviewAnalyticsQuerySet(AnalyticsQuerySet):
