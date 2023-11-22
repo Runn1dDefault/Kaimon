c4eeded63684caa4a509862b310bbb3226fb5087
Runn1dDefault <runniddefault@gmail.com>
2023-11-22 14:14:59 +0600

    remove rakuten scraping

--- deploy/sh/entrypoint.sh
+++ deploy/sh/entrypoint.sh
@@ -2,9 +2,9 @@
 
 pip install --upgrade pip
 
-echo 'Start making migrations...'
-python manage.py makemigrations --no-input
-python manage.py migrate --no-input
+#echo 'Start making migrations...'
+#python manage.py makemigrations --no-input
+#python manage.py migrate --no-input
 
 echo 'Collecting static files...'
 python manage.py collectstatic --no-input
