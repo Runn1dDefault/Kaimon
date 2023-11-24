ea74ab46a59a5261bd5bbd60383154f618bb984c
Runn1dDefault <runniddefault@gmail.com>
2023-11-24 14:32:45 +0600

    removed unused import

--- users/views.py
+++ users/views.py
@@ -1,7 +1,6 @@
 from django.utils.translation import gettext_lazy as _
 from rest_framework import generics, status, mixins
 from rest_framework.exceptions import ValidationError
-from rest_framework.parsers import JSONParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
