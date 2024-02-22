977b9a333843bb1325caaf53276fdee0fbb96a0c
dinnurnarmatov <dinnur.narmatov@data-ox.com>
2024-02-23 05:18:59 +0600

    added inventory by ids view

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import SimpleRouter
 from .views import (
     CategoryViewSet, UserReviewViewSet, ProductsViewSet, ProductByCategoryView,
     ProductReviewsAPIView, ProductsSearchView, ProductReferenceView, NewProductsView, PopularProductsView,
-    ProductByIdsView
+    ProductByIdsView, InventoriesByIdsView
 )
 
 router = SimpleRouter()
@@ -14,6 +14,7 @@ router.register('my-reviews', UserReviewViewSet, basename='user-reviews')
 
 urlpatterns = [
     path('products-by-ids/', ProductByIdsView.as_view(), name='product-products-by-ids-list'),
+    path('inventory-by-ids/', InventoriesByIdsView.as_view(), name='product-inventory-by-ids-list'),
     path('products/new/', NewProductsView.as_view(), name='products-new'),
     path('products/popular/', PopularProductsView.as_view(), name='products-popular'),
     path('products/search/', ProductsSearchView.as_view(), name='products-search'),
