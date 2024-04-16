bece26131946b3f3c9102f743266d025c99f51e5
runnid <runniddefault@gmail.com>
2024-04-16 17:34:39 +0600

    changed Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,10 @@ pip install --upgrade pip
 echo 'Collecting static files...'
 python manage.py collectstatic --no-input
 
+echo 'Making migrations...'
+python manage.py makemigrations --no-input
+echo 'Migrate...'
+python manage.py migrate --no-input
+
 echo 'Running server...'
 gunicorn kaimon.wsgi:application --bind 0.0.0.0:8334
