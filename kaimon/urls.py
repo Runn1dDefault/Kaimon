11f45200644b26af8b6298fb3329f20e0fa7b48b
dinnurnarmatov <dinnur.narmatov@data-ox.com>
2024-05-06 18:04:28 +0600

    changed django admin url

--- kaimon/urls.py
+++ kaimon/urls.py
@@ -12,7 +12,7 @@ docs_urlpatterns = [
 ]
 
 urlpatterns = [
-    path('dev-admin/', admin.site.urls),
+    path('api/v1/admin/', admin.site.urls),
     path('api/v1/', include('products.urls')),
     path('api/v1/auth/', include('users.urls')),
     path('api/v1/orders/', include('orders.urls')),
